17408beaa1793a4c798fb087a3c81539c9808416
G.Sathvika <153293504+gsathvikareddy78@users.noreply.github.com>
2024-03-23 17:33:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let buttons = document.querySelectorAll('button');
 
 let string = '';
 let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        }
+    })
+})
