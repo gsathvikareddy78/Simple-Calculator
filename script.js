443db586a95dee550ac6a30f020ef00e0db3b1f5
G.Sathvika <153293504+gsathvikareddy78@users.noreply.github.com>
2024-03-23 17:44:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,5 +17,9 @@ arr.forEach(button => {
             string=string.substring(0,string.length-1);
             input.value=string;
         }
+         else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
     })
 })
